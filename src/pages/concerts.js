accba5ef398ea3ccb6da0db1c45e92810d7a7181
Kuznetsov <denis.kuznetsov01@metronom.com>
2019-11-22 17:03:13 +0100

    push  концерты

--- src/pages/concerts.js
+++ src/pages/concerts.js
@@ -53,66 +53,262 @@ export default class ConcertsPage extends React.Component
 
               <table className="concert-table">
                 <tbody>
+                    <a href = "https://www.doerken-stiftung.de/" target="_blank">
                     <tr>
                         <td>
                             <span className="concert-first-title">
-                                date
+                            {this.state.LANGUAGE.t('concert_01_date')}
                             </span>
                             <span className="concert-second-title">
-                                place
+                            {this.state.LANGUAGE.t('concert_01_place')}
                             </span>
                         </td>
-
+                
                         <td>
-                            Country, City  
+                        {this.state.LANGUAGE.t('concert_01_city')}
                         </td>
 
                         <td>
-                            <a>
-                                Additional
-                            </a>
+                            {this.state.LANGUAGE.t('concert_01_event')}
                         </td>
+                        
+                        <td class="click-raw">
+                            {this.state.LANGUAGE.t('concert_01_additional')}
+                        </td>    
                     </tr>
+                    </a>
+                    <a href = "https://www.folkwang-uni.de/home/" target="_blank">
                     <tr>
                         <td>
                             <span className="concert-first-title">
-                                date
+                            {this.state.LANGUAGE.t('concert_02_date')}
                             </span>
                             <span className="concert-second-title">
-                                place
+                            {this.state.LANGUAGE.t('concert_02_place')}
                             </span>
                         </td>
 
                         <td>
-                            Country, City  
+                        {this.state.LANGUAGE.t('concert_02_city')}  
                         </td>
 
                         <td>
-                            <a>
-                                Additional
-                            </a>
+                            {this.state.LANGUAGE.t('concert_02_event')}
+                        </td>
+                        <td>
+                            {this.state.LANGUAGE.t('concert_02_additional')}
                         </td>
                     </tr>
+                    </a>
+                    <a href = "https://www.doerken-stiftung.de/" target="_blank">
                     <tr>
                         <td>
                             <span className="concert-first-title">
-                            date
+                            {this.state.LANGUAGE.t('concert_03_date')}
                             </span>
                             <span className="concert-second-title">
-                                place
+                            {this.state.LANGUAGE.t('concert_03_place')}
                             </span>
                         </td>
 
                         <td>
-                            Country, City  
+                        {this.state.LANGUAGE.t('concert_03_city')}  
                         </td>
 
                         <td>
-                            <a>
-                                Additional
-                            </a>
+                            {this.state.LANGUAGE.t('concert_03_event')}
+                        </td>
+                        <td>
+                            {this.state.LANGUAGE.t('concert_03_additional')}
                         </td>
                     </tr>
+                    </a>
+                    <a href = "https://www.cse.ruhr/soziales-gefaehrdetenhilfe/standort/3-cafe-schliessfach/" target="_blank">
+                    <tr>
+                        <td>
+                            <span className="concert-first-title">
+                            {this.state.LANGUAGE.t('concert_04_date')}
+                            </span>
+                            <span className="concert-second-title">
+                            {this.state.LANGUAGE.t('concert_04_place')}
+                            </span>
+                        </td>
+
+                        <td>
+                        {this.state.LANGUAGE.t('concert_04_city')}  
+                        </td>
+
+                        <td>
+                            {this.state.LANGUAGE.t('concert_04_event')}
+                        </td>
+                        <td>
+                            {this.state.LANGUAGE.t('concert_04_additional')}
+                        </td>
+                    </tr>
+                    </a>
+                    <a href = "https://www.folkwang-uni.de/home/" target="_blank">
+                    <tr>
+                        <td>
+                            <span className="concert-first-title">
+                            {this.state.LANGUAGE.t('concert_05_date')}
+                            </span>
+                            <span className="concert-second-title">
+                            {this.state.LANGUAGE.t('concert_05_place')}
+                            </span>
+                        </td>
+
+                        <td>
+                        {this.state.LANGUAGE.t('concert_05_city')}  
+                        </td>
+
+                        <td>
+                            {this.state.LANGUAGE.t('concert_05_event')}
+                        </td>
+                        <td>
+                            {this.state.LANGUAGE.t('concert_05_additional')}
+                        </td>
+                    </tr>
+                    </a>
+                    <a href = "https://akgs-duesseldorf.lms.schulon.org/" target="_blank">
+                    <tr>
+                        <td>
+                            <span className="concert-first-title">
+                            {this.state.LANGUAGE.t('concert_06_date')}
+                            </span>
+                            <span className="concert-second-title">
+                            {this.state.LANGUAGE.t('concert_06_place')}
+                            </span>
+                        </td>
+
+                        <td>
+                        {this.state.LANGUAGE.t('concert_06_city')}  
+                        </td>
+
+                        <td>
+                            {this.state.LANGUAGE.t('concert_06_event')}
+                        </td>
+                        <td>
+                            {this.state.LANGUAGE.t('concert_06_additional')}
+                        </td>
+                    </tr>
+                    </a>
+                    <a href = "https://www.folkwang-uni.de/home/hochschule/ueber-folkwang/campus/campus-duisburg/" target="_blank">
+                    <tr>
+                        <td>
+                            <span className="concert-first-title">
+                            {this.state.LANGUAGE.t('concert_07_date')}
+                            </span>
+                            <span className="concert-second-title">
+                            {this.state.LANGUAGE.t('concert_07_place')}
+                            </span>
+                        </td>
+
+                        <td>
+                        {this.state.LANGUAGE.t('concert_07_city')}  
+                        </td>
+
+                        <td>
+                            {this.state.LANGUAGE.t('concert_07_event')}
+                        </td>
+                        <td>
+                            {this.state.LANGUAGE.t('concert_07_additional')}
+                        </td>
+                    </tr>
+                    </a>
+                    <a href = "https://www.konzertgesellschaft.com/" target="_blank">
+                    <tr>
+                        <td>
+                            <span className="concert-first-title">
+                            {this.state.LANGUAGE.t('concert_08_date')}
+                            </span>
+                            <span className="concert-second-title">
+                            {this.state.LANGUAGE.t('concert_08_place')}
+                            </span>
+                        </td>
+
+                        <td>
+                        {this.state.LANGUAGE.t('concert_08_city')}  
+                        </td>
+
+                        <td>
+                            {this.state.LANGUAGE.t('concert_08_event')}
+                        </td>
+                        <td>
+                            {this.state.LANGUAGE.t('concert_08_additional')}
+                        </td>
+                    </tr>
+                    </a>
+                    <a href = "https://www.schloss-benrath.de/" target="_blank">
+                    <tr>
+                        <td>
+                            <span className="concert-first-title">
+                            {this.state.LANGUAGE.t('concert_09_date')}
+                            </span>
+                            <span className="concert-second-title">
+                            {this.state.LANGUAGE.t('concert_09_place')}
+                            </span>
+                        </td>
+
+                        <td>
+                        {this.state.LANGUAGE.t('concert_09_city')}  
+                        </td>
+
+                        <td>
+                            {this.state.LANGUAGE.t('concert_09_event')}
+                        </td>
+                        <td>
+                            {this.state.LANGUAGE.t('concert_09_additional')}
+                        </td>
+                    </tr>
+                    </a>
+                    <a href = "https://www.schloss-benrath.de/" target="_blank">
+                    <tr>
+                        <td>
+                            <span className="concert-first-title">
+                            {this.state.LANGUAGE.t('concert_10_date')}
+                            </span>
+                            <span className="concert-second-title">
+                            {this.state.LANGUAGE.t('concert_10_place')}
+                            </span>
+                        </td>
+
+                        <td>
+                        {this.state.LANGUAGE.t('concert_10_city')}  
+                        </td>
+
+                        <td>
+                            {this.state.LANGUAGE.t('concert_10_event')}
+                        </td>
+                        <td>
+                            {this.state.LANGUAGE.t('concert_10_additional')}
+                        </td>
+                    </tr>
+                    </a>
+                    <a href = "https://www.da-kunsthaus.de/" target="_blank">
+                    <tr>
+                        <td>
+                            <span className="concert-first-title">
+                            {this.state.LANGUAGE.t('concert_11_date')}
+                            </span>
+                            <span className="concert-second-title">
+                            {this.state.LANGUAGE.t('concert_11_place')}
+                            </span>
+                        </td>
+
+                        <td>
+                        {this.state.LANGUAGE.t('concert_11_city')}  
+                        </td>
+
+                        <td>
+                            {this.state.LANGUAGE.t('concert_11_event')}
+                        </td>
+                        <td>
+                            {this.state.LANGUAGE.t('concert_11_additional')}
+                        </td>
+                    </tr>
+                    </a>
+
+                    
                 </tbody>
               </table>
 
