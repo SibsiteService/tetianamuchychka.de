6140f9b35db61733889645c026fc09a2ae76218b
Kuznetsov <denis.kuznetsov01@metronom.com>
2019-12-03 18:50:14 +0100

    foto

--- src/pages/photo.js
+++ src/pages/photo.js
@@ -15,26 +15,83 @@ import "../assets/css/photo.css";
 
 import photo1 from "../assets/images/Tetiana_05.jpg";
 import photo2 from "../assets/images/Tetiana_09.jpg";
-import photo3 from "../assets/images/Tetiana_04_reduced.jpg";
+import photo3 from "../assets/images/Tetiana_04.jpg";
 import photo4 from "../assets/images/concerts.png";
+import photo5 from "../assets/images/Tetiana_03_reduced.jpg";
+import photo6 from "../assets/images/Tetiana_06.jpg";
+import photo7 from "../assets/images/Tetiana_07.jpg";
+import photo8 from "../assets/images/Tetiana_13.jpg";
+import photo9 from "../assets/images/Tetiana_14.jpg";
+import photo10 from "../assets/images/Tetiana_15.jpg";
+import photo11 from "../assets/images/Tetiana_16.jpg";
+import photo12 from "../assets/images/Tetiana_17.jpg";
+import photo13 from "../assets/images/Tetiana_18.jpg";
+import photo14 from "../assets/images/Tetiana_20.jpg";
+import photo15 from "../assets/images/Tetiana_21.jpg";
+import photo16 from "../assets/images/Tetiana_22.jpg";
+import photo17 from "../assets/images/Tetiana_23.jpg";
 
 let photos = [
     {
       original: photo1,
-      description  : 'Сюда описание'
     },
     {
       original: photo2,
-      description  : 'Сюда описание'
     },
     {
       original: photo3,
-      description  : 'Сюда описание'
     },
     {
       original: photo4, 
-      description  : 'Сюда описание' 
     },
+    {
+      original: photo5, 
+    },
+    {
+      original: photo6, 
+    },
+    {
+      original: photo7, 
+    },
+    {
+      original: photo8, 
+    },
+    {
+      original: photo9, 
+    },
+    {
+      original: photo10, 
+    },
+    {
+      original: photo11,
+      description:"Photo: Stefan Pieper" 
+    },
+    {
+      original: photo12,
+      description:"Photo: Lilo Ingenlath-Gegic" 
+    },
+    {
+      original: photo13,
+      description:"Photo: Lilo Ingenlath-Gegic" 
+    },
+    {
+      original: photo14,
+      description:"Photo: Kulturgemeinde Finnentrop" 
+    },
+    {
+      original: photo15,
+      description:"Photo: Kulturgemeinde Finnentrop" 
+    },
+    {
+      original: photo16, 
+      description:"Photo: Lisa Kannenbrock"
+    },
+    {
+      original: photo17,
+      description:"Photo: Lisa Kannenbrock" 
+    },
+
+
 ]
 
 export default class PhotoPage extends React.Component
