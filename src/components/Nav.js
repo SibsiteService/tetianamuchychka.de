8e5bb22ef3db1215eb3a2f4af5ccd17e058abf18
SibsiteService <sibsiteservice@gmail.com>
2021-03-03 17:51:15 +0100

    konzerte added DE

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -94,8 +94,10 @@ export default class Nav  extends React.Component{
                             </div>
                         </div>
         
-        
-                        
+                        <div className="flex-column menu-item">
+                            <a href={"/concerts/?lang="+ LANGUAGE.currentLang()}>{LANGUAGE.t('Konzerte')}</a>
+                            <div className="underline"/>
+                        </div>
         
                         <div className="flex-column menu-item">
                             <a href={"/media/?lang="+ LANGUAGE.currentLang()}>{LANGUAGE.t('Media')}</a>
